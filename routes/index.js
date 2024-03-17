71b2d618cf64a09aedd6ebc581fc413b0ccaa3b6
Juan Sebastián Giraldo <90337681+juansgiraldoe@users.noreply.github.com>
2024-03-17 12:41:22 -0500

    [Update] - Enable pug. // Create layouts.

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,14 @@ const router = express.Router();
 //request (req) es lo que enviamos (la peticion).
 //response (res) es la respuesta por parte de express.
 router.get('/', ( req, res ) => {
-  res.send('Inicio.')
+  res.render('inicio')
 });
 
 router.get('/nosotros', ( req, res ) => {
-  res.render('nosotros')
+  const viajes = 'Viaje a alemania';
+  res.render('nosotros',{
+    viajes,
+  })
 });
 
 export default router;
