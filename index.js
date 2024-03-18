b402569e5d9424d5edd1d22030e219413e5fddfd
Juan Sebastián Giraldo <90337681+juansgiraldoe@users.noreply.github.com>
2024-03-18 14:22:53 -0500

    [Update] - Create & connect DB {mysql}.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 //-> (commonjs) === const express = require('express');
 import express from 'express';
 import router from './routes/index.js';
+import db from './config/db.js';
+
 
 const app = express();
+
+//Conectar la base de datos.
+db.authenticate()
+  .then(() => console.log('Base de datos conectada'))
+  .catch(error => console.log(error))
 
 //Habilitar PUG.
 app.set('view engine', 'pug');
