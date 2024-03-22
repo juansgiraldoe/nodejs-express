240f17fcf54f1f6b53bf06c3b66450fd104b46c9
Juan Sebastián Giraldo <90337681+juansgiraldoe@users.noreply.github.com>
2024-03-22 14:57:30 -0500

    [Update] - Environment Variables.

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 //Configuracion de la base de datos.
 import Sequelize from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config()
 
-const db = new Sequelize('agenciadeviajes', 'root', 'root', {
-  host: '127.0.0.1',
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   port: '3306',
   dialect: 'mysql',
   define: {
